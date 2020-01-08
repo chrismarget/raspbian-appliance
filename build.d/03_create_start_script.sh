ecfe8bbaf94c4e00ee9825a8f1a2b96f4189dc12
chris marget <5470536+chrismarget@users.noreply.github.com>
2020-01-08 17:09:39 -0500

    refactor 03_create_start_script.sh

--- build.d/03_create_start_script.sh
+++ build.d/03_create_start_script.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+. functions
+
 if [ -z "$BOOT_MNT" ] || [ ! -e "$BOOT_MNT" ]
 then
-  echo "/boot mount point unknown"
+  error "/boot mount point unknown"
 fi
 
 cp $(dirname $0)/data/rc.local $BOOT_MNT/rc.local
+
+cat > $BOOT_MNT/rc.local << EOF
+#!/bin/sh
+
+# Template start script
+# This script will be copied to the SD card at /boot/rc.local
+# On boot, it's called by /etc/rc.local
+
+EOF
