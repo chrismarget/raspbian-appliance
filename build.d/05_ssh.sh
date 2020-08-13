2a2d260e147b0f0aadb8eab4ffb34312f8d3bab6
chris marget <5470536+chrismarget@users.noreply.github.com>
2020-08-13 09:55:48 -0400

    support for authorized_keys

--- build.d/05_ssh.sh
+++ build.d/05_ssh.sh
@@ -3,5 +3,9 @@
 . $(dirname $0)/functions
 
 [ -z "$BOOT_MNT" ] && error "boot mount unknown"
+[ -z "$PROJECT_DIR" ] && error "project dir unknown"
 
-touch ${BOOT_MNT}/ssh
+touch "${BOOT_MNT}/ssh"
+
+AKF="${PROJECT_DIR}/authorized_keys"
+[ -f "$AKF" ] && cp "$AKF" "${BOOT_MNT}"
