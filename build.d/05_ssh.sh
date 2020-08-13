e8cadba40d250801c0e68e248c14861406c05269
chris marget <5470536+chrismarget@users.noreply.github.com>
2020-08-13 10:54:46 -0400

    improved ssh startup

--- build.d/05_ssh.sh
+++ build.d/05_ssh.sh
@@ -5,7 +5,10 @@
 [ -z "$BOOT_MNT" ] && error "boot mount unknown"
 [ -z "$PROJECT_DIR" ] && error "project dir unknown"
 
-if [ -f "${PROJECT_DIR}/ssh" ]
+if [ -f "${PROJECT_DIR}/ssh" ] ||\
+   [ -f "${PROJECT_DIR}/authorized_keys" ] ||\
+   [ -f "${PROJECT_DIR}/copy_to_sd_boot/ssh" ] ||\
+   [ -f "${PROJECT_DIR}/copy_to_sd_boot/authorized_keys" ]
 then
   touch "${BOOT_MNT}/ssh"
   AKF="${PROJECT_DIR}/authorized_keys"
