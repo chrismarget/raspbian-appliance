b91086a387727265033031c3532d9dba7a559575
cmarget <cmarget@mutualink.net>
2020-09-13 16:34:23 -0400

    build scripts handling of submodule situation

--- build.d/02_copy_files.sh
+++ build.d/02_copy_files.sh
@@ -2,6 +2,7 @@
 
 . $(dirname $0)/functions
 
+[ -n "$APPLIANCE_DIR" ] || error "APPLIANCE_DIR unknown"
 [ -n "$BOOT_MNT" ] || error "boot partition mount point unknown"
 [ -d "$BOOT_MNT" ] || error "boot partition should be mounted, but isn't"
 
@@ -11,14 +12,14 @@ sleep 2
 [ -n "$P3_MNT" ] && ([ -d "$P3_MNT" ] || error "partition 3 should be mounted, but isn't")
 [ -n "$P4_MNT" ] && ([ -d "$P4_MNT" ] || error "partition 4 should be mounted, but isn't")
 
-(cd $PROJECT_DIR/copy_to_sd_boot && tar cLf - --exclude README.md .) | (cd $BOOT_MNT; tar xf -)
+(cd $APPLIANCE_DIR/copy_to_sd_boot && tar cLf - --exclude README.md .) | (cd $BOOT_MNT; tar xf -)
 
 if [ -n "$P3_MNT" ]
 then
-  (cd $PROJECT_DIR/copy_to_sd_p3 && tar cLf - --exclude README.md .) | (cd $P3_MNT; tar xf -)
+  (cd $APPLIANCE_DIR/copy_to_sd_p3 && tar cLf - --exclude README.md .) | (cd $P3_MNT; tar xf -)
 fi
 
 if [ -n "$P4_MNT" ]
 then
-  (cd $PROJECT_DIR/copy_to_sd_p4 && tar cLf - --exclude README.md .) | (cd $P4_MNT; tar xf -)
+  (cd $APPLIANCE_DIR/copy_to_sd_p4 && tar cLf - --exclude README.md .) | (cd $P4_MNT; tar xf -)
 fi
--- build.d/05_ssh.sh
+++ build.d/05_ssh.sh
@@ -2,16 +2,16 @@
 
 . $(dirname $0)/functions
 
-[ -z "$BOOT_MNT" ] && error "boot mount unknown"
-[ -z "$PROJECT_DIR" ] && error "project dir unknown"
+[ -n "$BOOT_MNT" ] || error "boot mount unknown"
+[ -n "$APPLIANCE_DIR" ] || error "appliance dir unknown"
 
-if [ -f "${PROJECT_DIR}/ssh" ] ||\
-   [ -f "${PROJECT_DIR}/authorized_keys" ] ||\
-   [ -f "${PROJECT_DIR}/copy_to_sd_boot/ssh" ] ||\
-   [ -f "${PROJECT_DIR}/copy_to_sd_boot/authorized_keys" ]
+if [ -f "${APPLIANCE_DIR}/ssh" ] ||\
+   [ -f "${APPLIANCE_DIR}/authorized_keys" ] ||\
+   [ -f "${APPLIANCE_DIR}/copy_to_sd_boot/ssh" ] ||\
+   [ -f "${APPLIANCE_DIR}/copy_to_sd_boot/authorized_keys" ]
 then
   touch "${BOOT_MNT}/ssh"
-  AKF="${PROJECT_DIR}/authorized_keys"
+  AKF="${APPLIANCE_DIR}/authorized_keys"
   [ -f "$AKF" ] && cp "$AKF" "${BOOT_MNT}"
 fi
 exit 0
