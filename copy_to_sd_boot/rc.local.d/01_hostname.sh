f2a41919d2a80e5d5717ea294bf4ebea806c7c29
cmarget <cmarget@mutualink.net>
2020-09-15 21:42:51 -0400

    improve hostname change

--- copy_to_sd_boot/rc.local.d/01_hostname.sh
+++ copy_to_sd_boot/rc.local.d/01_hostname.sh
@@ -2,6 +2,7 @@
 
 if [ -e /boot/hostname ]
 then
+  hostname "$(cat /boot/hostname)"
   raspi-config nonint do_hostname "$(cat /boot/hostname)"
   mkdir -p /boot/done
   mv /boot/hostname /boot/done
