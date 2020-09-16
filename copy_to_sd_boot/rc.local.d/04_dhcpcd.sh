93fb6d4c9cb79b7b34e4f6017e1daf96505bad2c
cmarget <cmarget@mutualink.net>
2020-09-16 16:40:43 -0400

    fix IP address on first boot

--- copy_to_sd_boot/rc.local.d/04_dhcpcd.sh
+++ copy_to_sd_boot/rc.local.d/04_dhcpcd.sh
@@ -5,4 +5,10 @@ then
   cat /boot/dhcpcd.conf >> /etc/dhcpcd.conf
   mkdir -p /boot/done
   mv /boot/dhcpcd.conf /boot/done
+  systemctl stop dhcpcd
+  for dev in $(ip -4 -o addr list | grep -v ' lo ' | awk '{print $2}')
+  do
+    ip addr flush dev $dev
+  done
+  systemctl start dhcpcd
 fi
